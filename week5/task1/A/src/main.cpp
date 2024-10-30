093965aad17f92a4c371891c3dfb3a041a91fd38
RuzenkovAndrew <ruzenkovandreu@gmail.com>
2024-10-30 21:55:34 +0300

    Add files via upload

---
+++ week5/task1/A/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "../include/calculations.hpp"
+
+int main() {
+    std::string filename = "/home/andry/volodin/week5/task1/A/src/text.txt"; // Имя файла для чтения
+    readFile(filename); // Читаем файл
+    return 0; // Успешное завершение программы
+}
+
